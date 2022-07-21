4cfa3769a8e80ecd6bd92754f54d0194f1e32e2f
эмиль ахметханов <emil.ah@yandex.ru>
2022-07-22 01:10:22 +0300

    Добавьте файлы проекта.

---
+++ Task 10/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10
+{
+    class Angle
+    {
+        public double degree;
+        public double minute;
+        public double second;
+        public bool isCorrect = true;
+        public double Degree
+        {
+            get
+            {
+                return degree;
+            }
+            set
+            {
+                if (degree < 0 || degree >=359)
+                    isCorrect = false;
+                else
+                    degree = value;
+            }
+        }
+        public double Minute
+        {
+            get
+            {
+                return minute;
+            }
+            set
+            {
+                if (minute < 0 || minute >=60)
+                    isCorrect = false;
+                else
+                    Degree += value;
+                minute = value;
+            }
+        }
+        public double Second
+        {
+            get
+            {
+                return second;
+            }
+            set
+            {
+                if (second < 0 || second >= 60)
+                    isCorrect = false;
+                else
+                    Minute += value;
+                second = value;
+            }
+        }
+        public double ToRadian()
+        {
+            return ((second / 3600 + minute / 60 + degree)* Math.PI / 180);
+        }
+    }
+}
+
---
+++ Task 10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Angle ang;
+            //do
+            //{
+                Console.WriteLine("Введите данные");
+                double degree_ = Convert.ToDouble(Console.ReadLine());
+                double minute_ = Convert.ToDouble(Console.ReadLine());
+                double second_ = Convert.ToDouble(Console.ReadLine());
+                //ang = new Angle(degree_,minute_,second_);
+                Angle ang = new Angle() { degree = degree_, minute = minute_, second = second_ };
+            //}
+            //while (ang.isCorrect == false);
+
+            double totalAngle = ang.ToRadian();
+            Console.WriteLine(totalAngle);
+            Console.ReadKey();
+        }
+    }
+}
